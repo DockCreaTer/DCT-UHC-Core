cc137accbff2d6b18f351c9a54ae2d01a04bbf84
Savion <saviondeavault@gmail.com>
2016-10-05 21:38:09 -0400

    Yuh

--- src/CoreUHC/tasks/GameTick.php
+++ src/CoreUHC/tasks/GameTick.php
@@ -35,14 +35,12 @@ private $plugin;
 		$this->getPlugin()->match->setTime(($time - 1));
 		foreach($this->getServer()->getOnlinePlayers() as $p){
 			if($this->getPlugin()->match->getStatus() === Main::GRACE){
-				$p->sendTip("Grace ends in: ".gmdate("i:s", $time));
 				if($time === 0){
 					$this->getPlugin()->match->setStatus(Main::PVP);
 					$this->getServer()->broadcastMessage(Main::PREFIX."Grace is now over. PvP is enabled!");
 				}
 			}
 			if($this->getPlugin()->match->getStatus() === Main::PVP){
-				//todo: border
 			}
 		}
 	}
